7ca21aa135de5d8094780432126318498e0ae0e4
Oustaa <otailaba98@gmail.com>
2024-07-20 22:50:20 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,40 @@
-import * as fs from "fs";
+import * as fs from "node:fs";
+import zlib from "node:zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
   Init = "init",
+  Catfile = "cat-file",
 }
 
 switch (command) {
   case Commands.Init:
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    console.log("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
+    console.log("Initialized git directory");
     fs.mkdirSync(".git", { recursive: true });
     fs.mkdirSync(".git/objects", { recursive: true });
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
-    console.log("Initialized git directory");
     break;
+  case Commands.Catfile:
+    if (args.length !== 3) {
+      console.error("Usage: git cat-file <object-type> <object-id>");
+      process.exit(1);
+    }
+    const blobDir = args[2].substring(0, 2);
+    const blobFile = args[2].substring(2);
+    const blob = fs.readFileSync(`./.git/objects/${blobDir}/${blobFile}`);
+    const decompressedBuffer = zlib.unzipSync(blob);
+
+    const nullByteIndex = decompressedBuffer.indexOf(0);
+    const blobContent = decompressedBuffer
+      .subarray(nullByteIndex + 1)
+      .toString();
+
+    process.stdout.write(blobContent);
+    break;
+
   default:
     throw new Error(`Unknown command ${command}`);
 }
